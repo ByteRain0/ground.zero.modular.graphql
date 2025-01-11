7172150189d6a267ce7f559f6618760c5bca5139
vasileoleinic <vasilii.oleinic@indrivo.com>
2025-01-11 17:00:00 +0200

    Add adr-0017-data-loaders.md. Add startup config for only Japanese.Api.

--- src/Host/App.Host/Program.cs
+++ src/Host/App.Host/Program.cs
@@ -1,8 +1,6 @@
-using Projects;
-
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder
+var collector = builder
     .AddOpenTelemetryCollector("collector", "otel-config.yaml")
     .WithAppForwarding()
     .WithLifetime(ContainerLifetime.Persistent);
@@ -19,31 +17,35 @@ var defaultDb = postgres.AddDatabase("default-db");
 var migrationService = builder.AddProject<Projects.Japanese_Api_MigrationService>("migration-service")
     .WithReference(defaultDb);
 
-var keycloack = builder
+var keycloak = builder
     .AddKeycloak(name: "keycloack", port: 8080)
     .WithDataVolume()
     .WithRealmImport("../../../configurations/keycloack")
     .WithExternalHttpEndpoints()
     .WithLifetime(ContainerLifetime.Persistent);
 
-var cache = builder.AddRedis("cache", port: 6379)
-    .WithDataVolume()
-    .WithLifetime(ContainerLifetime.Persistent)
-    .WithRedisInsight();
-
 var japaneseApi = builder
     .AddProject<Projects.Japanese_Api>("manga-api")
     .WithReference(defaultDb).WaitFor(defaultDb)
-    .WithReference(keycloack).WaitFor(keycloack)
+    .WithReference(keycloak).WaitFor(keycloak)
     .WithReference(migrationService).WaitFor(migrationService);
 
-var ratingApi = builder
-    .AddProject<Projects.Rating_Api>("rating-api")
-    .WithReference(cache).WaitFor(cache);
+bool.TryParse(Environment.GetEnvironmentVariable("START_WITH_RATING_API"), out var startWithRatingApi );
+if (startWithRatingApi)
+{
+    var cache = builder.AddRedis("cache", port: 6379)
+        .WithDataVolume()
+        .WithLifetime(ContainerLifetime.Persistent)
+        .WithRedisInsight();
+    
+    var ratingApi = builder
+        .AddProject<Projects.Rating_Api>("rating-api")
+        .WithReference(cache).WaitFor(cache);
 
-builder.AddFusionGateway<Projects.Gateway>("fusion-gateway")
-    .WithSubgraph(japaneseApi)
-    .WithSubgraph(ratingApi);
+    builder.AddFusionGateway<Projects.Gateway>("fusion-gateway")
+        .WithSubgraph(japaneseApi)
+        .WithSubgraph(ratingApi);
+}
 
 builder
     .Build()
